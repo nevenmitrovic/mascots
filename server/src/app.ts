e9978b87e85f02636767d7e6009f615192e86853
neven <nevenmitrovic4@gmail.com>
2025-03-31 20:09:52 +0200

    initialized error handling

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,13 +1,25 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import morgan from "morgan";
 
 import { env } from "config/env";
+import { errorMiddleware } from "middlewares/error.middleware";
+
+import { HttpError } from "errors/http.error";
 
 class App {
   private app = express();
 
   constructor() {
     this.initializeMiddlewares();
+    this.initializeErrorHandling();
+  }
+
+  private initializeErrorHandling() {
+    this.app.use(
+      (err: HttpError, req: Request, res: Response, _next: NextFunction) => {
+        errorMiddleware(err, req, res, _next);
+      }
+    );
   }
 
   private initializeMiddlewares(): void {
