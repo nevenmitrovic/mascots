1bf6644fff511d97eebed4e84ad138286e8a3c83
Igor-Sasic <igor.sasic811@gmail.com>
2025-04-09 18:52:04 +0200

    axios locations

--- client/src/hooks/useToastMessage.tsx
+++ client/src/hooks/useToastMessage.tsx
@@ -3,7 +3,7 @@ import { useState, useCallback } from "react";
 
 interface UseToastMessage {
   showToast: (message: string) => void;
-  ToastComponent: React.FC;
+  ToastComponent: React.FC<{ severity?: AlertColor }>;
 }
 
 const useToastMessage = (): UseToastMessage => {
@@ -23,14 +23,16 @@ const useToastMessage = (): UseToastMessage => {
     []
   );
 
-  const ToastComponent: React.FC = () => (
+  const ToastComponent: React.FC<{ severity?: AlertColor }> = ({
+    severity = "info",
+  }) => (
     <Snackbar
       open={open}
       autoHideDuration={4000}
       onClose={handleClose}
       anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
     >
-      <Alert onClose={handleClose} sx={{ width: "100%" }}>
+      <Alert onClose={handleClose} sx={{ width: "100%" }} severity={severity}>
         {message}
       </Alert>
     </Snackbar>
--- client/src/utils/types.ts
+++
@@ -1,7 +0,0 @@
-export type Location = {
-  id: string;
-  location: string;
-  name: string;
-  adress: string;
-  phone: string;
-};
