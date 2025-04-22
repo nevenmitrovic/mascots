980052dc3ecbccc5ca39433aeb9646d369e0e93e
nevenmitrovic <nevenmitrovic4@gmail.com>
2025-04-22 12:28:48 +0200

    animator service methods

--- server/src/animators/animator.service.ts
+++ server/src/animators/animator.service.ts
@@ -1,6 +1,12 @@
+import { Types } from "mongoose";
+
 import { AnimatorRepository } from "animators/animator.repository";
 
 import { ErrorHandlerService } from "services/error-handler.service";
+
+import { BadRequestError } from "errors/bad-request.error";
+import { NotFoundError } from "errors/not-found.error";
+
 import {
   IAnimator,
   IAnimatorDocument,
@@ -15,6 +21,83 @@ export class AnimatorService {
     try {
       const res = await this.animatorRepository.createAnimator(data);
       return { message: "Animator created successfully", data: res };
+    } catch (err) {
+      const error = this.errorHandler.handleError(err as Error);
+      throw error;
+    }
+  }
+
+  async getAnimators(): Promise<IAnimatorDocument[]> {
+    try {
+      const res = await this.animatorRepository.getAnimators();
+      return res;
+    } catch (err) {
+      const error = this.errorHandler.handleError(err as Error);
+      throw error;
+    }
+  }
+
+  async getAnimatorById(id: string): Promise<IAnimatorDocument> {
+    try {
+      if (!Types.ObjectId.isValid(id)) {
+        throw new BadRequestError("invalid id");
+      }
+
+      const animator = await this.animatorRepository.getAnimatorById(id);
+      if (!animator) {
+        throw new NotFoundError("animator not found");
+      }
+
+      return animator;
+    } catch (err) {
+      const error = this.errorHandler.handleError(err as Error);
+      throw error;
+    }
+  }
+
+  async updateAnimator(
+    id: string,
+    data: IAnimator
+  ): Promise<IAnimatorMessageResponse> {
+    try {
+      if (!data) {
+        throw new BadRequestError("data not provided");
+      }
+      if (!Types.ObjectId.isValid(id)) {
+        throw new BadRequestError("invalid id");
+      }
+
+      const updatedAnimator = await this.animatorRepository.updateAnimator(
+        id,
+        data
+      );
+      if (!updatedAnimator) {
+        throw new NotFoundError("animator not found");
+      }
+      return {
+        message: "animator updated successfully",
+        data: updatedAnimator,
+      };
+    } catch (err) {
+      const error = this.errorHandler.handleError(err as Error);
+      throw error;
+    }
+  }
+
+  async deleteAnimator(id: string): Promise<IAnimatorMessageResponse> {
+    try {
+      if (!Types.ObjectId.isValid(id)) {
+        throw new BadRequestError("invalid id");
+      }
+
+      const deletedAnimator = await this.animatorRepository.deleteAnimator(id);
+      if (!deletedAnimator) {
+        throw new NotFoundError("animator not found");
+      }
+      return {
+        message: "animator deleted successfully",
+        data: deletedAnimator,
+      };
     } catch (err) {
       const error = this.errorHandler.handleError(err as Error);
       throw error;
