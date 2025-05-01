8a2714493eb922b49e5dc22ce3a3d91bbae42146
Igor-Sasic <igor.sasic811@gmail.com>
2025-05-01 22:40:28 +0200

    util function to map data for event select

--- client/src/utils/helperFunctions.ts
+++ client/src/utils/helperFunctions.ts
@@ -1,5 +1,22 @@
-import { type IEvent } from "../types/eventTypes";
+import { type IEvent } from "types/eventTypes";
+import { type LocationDocument } from "types/locationTypes";
+import { type MascotDocument } from "types/mascotTypes";
+import { type AnimatorDocument } from "types/animatorsTypes";
 
+type AnimatorSelectProps =
+  | LocationDocument[]
+  | MascotDocument[]
+  | AnimatorDocument[];
+
+//function to map data to display select input for create/edit event form 
+export const mapSelectedData = (data: AnimatorSelectProps) => {
+  return data.map((item) => ({
+    title: "name" in item ? item.name : item.username,
+    value: item._id,
+  }));
+};
+
+//default values for the form
 export const defaultValues = <T>(inputs: T[]) => {
   const obj: Record<string, string | string[]> = {};
   inputs.forEach((input: any) => {
@@ -38,3 +55,4 @@ export function mapEventsToCalendar(events: IEvent[]) {
     };
   });
 }
+
