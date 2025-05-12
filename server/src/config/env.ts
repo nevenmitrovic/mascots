ecb1abc4c1bc0d6a604f76cf21ffd5867f4ca329
nevenmitrovic <nevenmitrovic4@gmail.com>
2025-05-12 17:41:59 +0200

    login validation and getAnimatorByUsername

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -1,9 +1,10 @@
 import { config } from "dotenv";
-import { cleanEnv, port, url } from "envalid";
+import { cleanEnv, port, url, str } from "envalid";
 
 config({ path: ".env" });
 
 export const env = cleanEnv(process.env, {
   PORT: port({ default: 5000 }),
   MONGODB_URI_DEVELOPMENT: url(),
+  SECRET_KEY: str(),
 });
