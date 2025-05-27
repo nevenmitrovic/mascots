c15b1050adc9f0d491efbd38f3de587e1123f243
nevenmitrovic <nevenmitrovic4@gmail.com>
2025-05-27 13:14:52 +0200

    vite config updated

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
-
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react(), tsconfigPaths()],
-})
+  preview: {
+    port: 8081,
+    strictPort: true,
+  },
+  server: {
+    port: 8081,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8081",
+  },
+});
